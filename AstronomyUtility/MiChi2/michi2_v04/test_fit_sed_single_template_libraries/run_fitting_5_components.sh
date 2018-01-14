d1ff1c7fa3ad963c5b4c9986c9c6db71515eddc9
1054 <liudz1054@gmail.com>
2018-01-14 14:23:13 +0100

    Updates

---
+++ AstronomyUtility/MiChi2/michi2_v04/test_fit_sed_single_template_libraries/run_fitting_5_components.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#
+/isaac/u/dzliu/Cloud/Github/Crab.Toolkit.michi2/bin/michi2-run-fitting-5-components $@
---
+++ AstronomyUtility/MiChi2/michi2_v04/test_fit_sed_single_template_libraries/run_plotting_5_components.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#
+/isaac/u/dzliu/Cloud/Github/Crab.Toolkit.michi2/bin/michi2-plot-results-of-fitting-5-components $@
